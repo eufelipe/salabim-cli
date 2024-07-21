852925554afdcdf76c3f29155461568ace43d185
Felipe Rosas <contato@eufelipe.com>
2024-07-21 14:38:13 -0300

    feat: add script for setup linting

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,7 @@
 const { program } = require('commander');
 const { setupKeystore } = require('../lib/setupKeystore');
 const { setupFastlane } = require('../lib/setupFastlane');
+const { setupLinting } = require('../lib/setupLinting');
 
 program
   .command('setup-keystore')
@@ -13,9 +14,16 @@ program
 
 program
   .command('setup-fastlane')
-  .description('Setup Fastlane for the project')
+  .description('Setup Fastlane for Android project')
   .action(() => {
     setupFastlane();
+  });
+
+program
+  .command('setup-linting')
+  .description('Setup ESLint and Prettier for the project')
+  .action(() => {
+    setupLinting();
   });
 
 program.parse(process.argv);
