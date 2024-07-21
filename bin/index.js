6859b448db34d918b11ef39f7e911bd8e977e4c7
Felipe Rosas <contato@eufelipe.com>
2024-07-21 13:45:48 -0300

    feat: add script for setup fastlane for android

--- bin/index.js
+++ bin/index.js
@@ -2,12 +2,20 @@
 
 const { program } = require('commander');
 const { setupKeystore } = require('../lib/setupKeystore');
+const { setupFastlane } = require('../lib/setupFastlane');
 
 program
   .command('setup-keystore')
   .description('Setup keystore for Android project')
   .action(() => {
     setupKeystore();
+  });
+
+program
+  .command('setup-fastlane')
+  .description('Setup Fastlane for the project')
+  .action(() => {
+    setupFastlane();
   });
 
 program.parse(process.argv);
