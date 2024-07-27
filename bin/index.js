6ecdb976f69f433993649c1d111c072fc4189ff2
Felipe Rosas <contato@eufelipe.com>
2024-07-27 13:38:30 -0300

    chore: add version command

--- bin/index.js
+++ bin/index.js
@@ -1,46 +1,53 @@
 #!/usr/bin/env node
 
-const { program } = require('commander');
-const { setupKeystore } = require('../lib/setupKeystore');
-const { setupLinting } = require('../lib/setupLinting');
-const { setupFastlane } = require('../lib/setupFastlane');
-const { setupCspell } = require('../lib/setupCspell');
-const { setupCodeQuality } = require('../lib/setupCodeQuality');
+const { program } = require("commander");
+const packageJson = require("../package.json");
+
+const { setupKeystore } = require("../lib/setupKeystore");
+const { setupLinting } = require("../lib/setupLinting");
+const { setupFastlane } = require("../lib/setupFastlane");
+const { setupCspell } = require("../lib/setupCspell");
+const { setupCodeQuality } = require("../lib/setupCodeQuality");
+
+program.version(
+  packageJson.version,
+  "-v, --version",
+  "output the current version"
+);
 
 program
-  .command('setup-keystore')
-  .description('Setup keystore for Android project')
+  .command("setup-keystore")
+  .description("Setup keystore for Android project")
   .action(() => {
     setupKeystore();
   });
 
 program
-  .command('setup-linting')
-  .description('Setup ESLint and Prettier')
+  .command("setup-linting")
+  .description("Setup ESLint and Prettier")
   .action(() => {
     setupLinting();
   });
 
 program
-  .command('setup-fastlane')
-  .description('Setup Fastlane for Android project')
+  .command("setup-fastlane")
+  .description("Setup Fastlane for Android project")
   .action(() => {
     setupFastlane();
   });
 
 program
-  .command('setup-cspell')
-  .description('Setup Cspell for the project')
+  .command("setup-cspell")
+  .description("Setup Cspell for the project")
   .action(() => {
     setupCspell();
   });
 
-  program
-  .command('setup-code-quality')
-  .description('Setup lint-staged, commitlint, and husky')
+program
+  .command("setup-code-quality")
+  .description("Setup lint-staged, commitlint, and husky")
   .action(() => {
     setupCodeQuality();
   });
-
 
 program.parse(process.argv);
